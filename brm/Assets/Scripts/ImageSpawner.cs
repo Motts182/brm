c02a1f37f349ae9926314ee6f06a5fbc8b11be36
Nahuel Castillo <nahushadow@gmail.com>
2016-11-10 18:03:21 -0300

    Nahue: Agregue el spawner de Img y un prototipo de ImgFlow

---
+++ brm/Assets/Scripts/ImageSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImageSpawner : MonoBehaviour {
+
+	public int cantImg;
+	public GameObject ImgRef;
+
+
+	void Start (){
+	
+		for (int i = 1; i <= cantImg; i++) {
+
+			GameObject img = Instantiate (ImgRef, this.transform.position, Quaternion.identity) as GameObject;
+
+			img.GetComponent<ImageMovement> ().ImgPath =  "ImagePath" + i;
+
+		}
+	}
+}
