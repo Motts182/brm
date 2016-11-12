19c465c7a8fa0282f16f4a75ddd7506ae10aba50
Nahuel Castillo <nahushadow@gmail.com>
2016-11-12 05:00:00 -0300

    resolve conflict (separados los scripts de spawners)

---
+++ brm/Assets/Scripts/ImageSpawnerNahue.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImageSpawnerNahue : MonoBehaviour {
+
+	public int cantImg;
+	public GameObject[] imgRef;
+
+
+	void Start (){
+
+		for (int i = 0; i < cantImg; i++) {
+
+			GameObject img = Instantiate (imgRef[Random.Range(0,2)], this.transform.position, Quaternion.identity) as GameObject;
+
+			img.GetComponent<ImageMovement> ().ImgPath = "ImagePath" + i;
+
+		}
+	}
+}
