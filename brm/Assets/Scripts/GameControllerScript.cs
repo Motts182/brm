2a43f90d248e3517bfaecb7c463a841e68f9df08
BLACKROCK92 <perezdaffunchio@gmail.com>
2016-11-14 22:10:01 -0300

    La concha de los www

---
+++ brm/Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameControllerScript : MonoBehaviour
+{
+
+    //[SerializeField]
+    //BrowserLogin loginRef;
+    public string userToken;
+    string hashtag;
+    string url;
+    string maxLength;
+    string minLength;
+    public JsonData data;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        RequestByHashtag("snow");
+    }
+
+    public IEnumerator WaitforRequest(WWW www)
+    {
+        yield return www;
+        if(www.error == null)
+        {
+            Debug.Log("WWW OK!");
+            JSONParse(www);
+        }
+        else
+        {
+            Debug.Log("WWW ERROR !");
+        }
+    }
+
+    public void RequestByHashtag(string hashtag)
+    {
+        url = "https://api.instagram.com/v1/tags/" + hashtag + "/media/recent?access_token=" + userToken;
+        WWW www = new WWW(url);
+        StartCoroutine(WaitforRequest(www));
+    }
+
+    void JSONParse(WWW www)
+    {
+        //print(www.text);
+        data = JsonMapper.ToObject(www.text);
+        WWW imglink = new WWW(data["data"][0]["images"]["standard_resolution"]["url"].ToString());
+        
+    }
+
+}
--- brm/assets/scripts/BrowserLogin.cs
+++ brm/assets/scripts/BrowserLogin.cs
@@ -5,13 +5,15 @@ using UnityEngine.UI;
 public class BrowserLogin : MonoBehaviour
 {
 
-    string clientID = "78d3e74cb7b646fab70a8e381c2d487a";
+    string clientID = "e2b157c42b814567a3fd7d4d133f2890";
     string redirectUri = "http://www.brm.com.co/themes/site_themes/brm_site/default_site/Site_brm.group/img/logo.png";
-    public string userToken;
+    private string userToken;
     string igPage;
     public Text tokenTxt;
     public GameObject loginBtn;
     public GameObject nextBtn;
+
+
 
     void Awake()
     {
@@ -34,5 +36,29 @@ public class BrowserLogin : MonoBehaviour
         yield return new WaitForSeconds(x);
         InAppBrowser.CloseBrowser();
     }
-    
+
+    public IEnumerator getprofilepic()
+    {
+        WWW request = new WWW("https://api.instagram.com/v1/users/self/?access_token=" + userToken);
+        yield return request;
+
+    }
+
+    void GetToken()
+    {
+        if (userToken != null)
+        {
+            GameControllerScript gcs = FindObjectOfType<GameControllerScript>();
+            gcs.userToken = userToken;
+        }
+    }
+    public string UserToken
+    {
+        set
+        {
+            userToken = value;
+            GetToken();
+            
+        }
+    }
 }
