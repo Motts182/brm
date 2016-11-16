81b5e2c4eb72badd6b51689f873ffe4272cfba0a
Nahuel Castillo <nahushadow@gmail.com>
2016-11-16 18:00:07 -0300

    agregado el event trigger para las imagenes, el panel no funciona con los paths (no lo sigue)

---
+++ brm/Assets/Scripts/ImgGazeTriggerScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImgGazeTriggerScript : MonoBehaviour{
+	
+
+	public void OnGazeTrigger() {
+		//aqui deberia ir la logica de la seleccion de una imagen.
+
+		Debug.Log ("Has seleccionado una imagen");
+	}
+}
