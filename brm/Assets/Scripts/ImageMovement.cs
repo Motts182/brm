b76a74cb69fff44afb0a4c9b1bba68b7c9d90f0d
BLACKROCK92 <perezdaffunchio@gmail.com>
2016-11-17 17:49:30 -0300

    fran y nahue: limpiar carpeta prefabs, unificar spawners

--- brm/Assets/Scripts/ImageMovement.cs
+++ brm/Assets/Scripts/ImageMovement.cs
@@ -1,17 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ImageMovement : MonoBehaviour {
+public class ImageMovement : MonoBehaviour
+{
+    public string ImgPath;
 
+    void Start()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(ImgPath), "time", 10f, "easetype", iTween.EaseType.easeInOutSine, "looktarget", GameObject.FindGameObjectWithTag("MainCamera").transform));
+    }
 
-	public string ImgPath;
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, GameObject.FindGameObjectWithTag("MainCamera").transform.position) <= 2)
+        {
+            this.GetComponentInChildren<MeshRenderer>().enabled = false;
+        }
+        else
+        {
+            this.GetComponentInChildren<MeshRenderer>().enabled = true;
+        }
 
-	// Use this for initialization
-	void Start () {
-			
-
-		iTween.MoveTo (gameObject, iTween.Hash("path", iTweenPath.GetPath(ImgPath), "time", 10f, "easetype", iTween.EaseType.easeInOutSine, "looktarget", GameObject.FindGameObjectWithTag("MainCamera").transform));
-	}
-	
+    }
 
 }
