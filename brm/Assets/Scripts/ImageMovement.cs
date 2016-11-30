5d3e9d0f8bcc92e9e8954bc39cacf2d149099b40
BLACKROCK92 <perezdaffunchio@gmail.com>
2016-11-29 21:40:37 -0300

    fran: 29-11

--- brm/Assets/Scripts/ImageMovement.cs
+++ brm/Assets/Scripts/ImageMovement.cs
@@ -5,23 +5,69 @@ using UnityEngine.UI;
 public class ImageMovement : MonoBehaviour
 {
     public string ImgPath;
+    Image imgScript;
+    float aux;
+    public float decAlpha = 0.005f;
+    bool alphaOn;
+
+    void Awake()
+    {
+        aux = 1f;
+        imgScript = GetComponentInChildren<Image>();
+    }
 
     void Start()
     {
-        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(ImgPath), "time", 10f,"loopType", "loop", "easetype", iTween.EaseType.easeInOutSine, "looktarget", GameObject.FindGameObjectWithTag("MainCamera").transform));
+        iTween.FadeTo(gameObject, iTween.Hash(
+                                                "alpha", 1f,
+                                                "time", 1f,
+                                                "delay", 9f,
+                                                "onstart", "formatAlpha"
+                                                //"oncomplete", "alphaOnMetod1"
+                                                ));
+        iTween.ScaleTo(gameObject, iTween.Hash(
+                                                "scale", new Vector3(2f, 2f, 1f),
+                                                "time", 10f,
+                                                "loopType", "loop",
+                                                "easetype", iTween.EaseType.easeInOutSine
+                                                ));
+        iTween.MoveTo(gameObject, iTween.Hash(
+                                                "path", iTweenPath.GetPath(ImgPath),
+                                                "time", 10f,
+                                                "loopType", "loop",
+                                                "easetype", iTween.EaseType.easeInOutSine,
+                                                "looktarget", GameObject.FindGameObjectWithTag("MainCamera").transform
+                                                ));
     }
+
+
+    void formatAlpha() {
+        aux = 1;
+    }
+
+    void alphaOnMetod(bool val) {
+        print(val);
+        alphaOn = val;
+    }
+
+    void alphaOnMetod1()
+    {
+        print(true);
+        alphaOn = true;
+    }
+
+
 
     private void FixedUpdate()
     {
-        if(GameObject.Find("MainCamera") != null)
-        {
-            if (Vector3.Distance(this.gameObject.transform.position, GameObject.Find("MainCamera").transform.position) >= 5)
-            {
-                GetComponentInChildren<Image>().enabled = false;
-            }
-            else
-            {
-                GetComponentInChildren<Image>().enabled = true;
+        if (alphaOn) {
+            aux -= decAlpha;
+            imgScript.material.SetColor("_Color", new Color(1, 1, 1, aux));
+            if (aux <= 0) {
+                print("aux " + aux);
+
+                formatAlpha();
+                alphaOnMetod(false);
             }
         }
     }
---
+++ brm/Assets/Scripts/ImageMovement2.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ImageMovement2 : MonoBehaviour
+{
+    public bool rotate = false;
+    void Start()
+    {
+        iTween.ScaleTo(gameObject, iTween.Hash(
+                                                "scale", new Vector3(1.3f, 1.3f, 1f),
+                                                "delay", 1f,
+                                                "time", 3f,
+                                                "loopType", "pingPong",
+                                                "easetype", iTween.EaseType.easeInOutSine,
+                                                "oncomplete", "rotationTrue",
+                                                "onstart", "rotationFalse"
+                                                ));
+        Destroy(gameObject, 5f);
+    }
+
+    void rotationFalse()
+    {
+        rotate = false;
+    }
+
+    void rotationTrue()
+    {
+        rotate = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if(rotate)
+        transform.Rotate(0, Time.time*0.5f, 0);
+    }
+    
+}
