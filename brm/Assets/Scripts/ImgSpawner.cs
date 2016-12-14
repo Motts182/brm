0ae87a7c67d3ed3dffc3f0645ec8b715f14eb907
Nahuel Castillo <nahushadow@gmail.com>
2016-12-14 16:42:41 -0300

    Rivers Retocados

--- brm/Assets/Scripts/ImgSpawner.cs
+++ brm/Assets/Scripts/ImgSpawner.cs
@@ -7,49 +7,40 @@ using UnityEngine.SceneManagement;
 public class ImgSpawner : MonoBehaviour
 {
 
-    public int cantImg;
-    public GameObject imgRef;
-    public List<Sprite> spriteList = new List<Sprite>();
-    public List<string> imgLinkList = new List<string>();
-    public List<GameObject> spawnPoints = new List<GameObject>();
-    private void Awake()
-    {
-        if (GameObject.FindGameObjectsWithTag("SpawnPoint") != null)
-        {
-            foreach (GameObject go in GameObject.FindGameObjectsWithTag("SpawnPoint"))
-            {
-                spawnPoints.Add(go);
-            }
-        }
-    }
-    public IEnumerator Spawn()
-    {
-        if (SceneManager.GetActiveScene().name != "ImageRiver4")
-        {
-            for (int i = 0; i < cantImg; i++)
-            {
-                var l = Random.Range(0, spriteList.Count);
-                GameObject img = Instantiate(imgRef, this.transform.position, Quaternion.identity) as GameObject;
-                img.GetComponentInChildren<Image>().sprite = spriteList[l];
-                img.GetComponent<ImageMovement>().ImgPath = "ImagePath" + i;
-                img.GetComponent<ImageScript>().imglink = imgLinkList[l];
-                img.transform.SetParent(this.transform);
-                yield return new WaitForSeconds(5f);
-            }
-        }
-        else
-        {
-            for (int i = 0; i < spawnPoints.Count; i++)
-            {
-                var l = Random.Range(0, spriteList.Count);
-                GameObject img = Instantiate(imgRef, spawnPoints[i].transform.position, Quaternion.identity) as GameObject;
-                img.GetComponentInChildren<Image>().sprite = spriteList[l];
-                img.GetComponent<ImageScript>().imglink = imgLinkList[l];
-                img.transform.SetParent(this.transform);
-            }
-            yield return new WaitForSeconds(10f);
-            StartCoroutine(Spawn());
-        }
+	public int cantImg;
+	public GameObject imgRef;
+	public List<Sprite> spriteList = new List<Sprite> ();
+	public List<string> imgLinkList = new List<string> ();
+	public List<GameObject> spawnPoints = new List<GameObject> ();
 
-    }
+	private void Awake ()
+	{
+		if (GameObject.FindGameObjectsWithTag ("SpawnPoint") != null) {
+			foreach (GameObject go in GameObject.FindGameObjectsWithTag("SpawnPoint")) {
+				spawnPoints.Add (go);
+			}
+		}
+	}
+
+	public IEnumerator Spawn ()
+	{
+		
+		for (int i = 0; i < spawnPoints.Count; i++) {
+			
+			var l = Random.Range (0, spriteList.Count);
+			GameObject img = Instantiate (imgRef, spawnPoints [i].transform.position, Quaternion.identity) as GameObject;
+			img.GetComponentInChildren<Image> ().sprite = spriteList [l];
+
+			if (SceneManager.GetActiveScene ().name != "ImageRiver3") {
+				
+				img.GetComponent<ImageMovement> ().ImgPath = "ImagePath" + i;
+			}
+
+			img.GetComponent<ImageScript> ().imglink = imgLinkList [l];
+			img.transform.SetParent (this.transform);
+		}
+		yield return new WaitForSeconds (10f);
+		StartCoroutine (Spawn ());
+
+	}
 }
